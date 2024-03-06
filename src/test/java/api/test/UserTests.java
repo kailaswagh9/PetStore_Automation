30f39ec9d3de4457c6c18e0086a82157cb69f6bd
kailaswagh9 <kailas.wagh189@gmail.com>
2024-03-06 17:33:57 +0530

    logs and extent report

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -2,7 +2,8 @@ package api.test;
 
 import static org.testng.Assert.assertEquals;
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -17,10 +18,12 @@ import io.restassured.response.Response;
 public class UserTests {
 	Faker faker;
 	User userPayload;
+	org.apache.logging.log4j.Logger logger;
 	@BeforeClass
 	public void setupData() {
 		faker = new Faker();
 		userPayload = new User();
+		
 		userPayload.setId(faker.idNumber().hashCode());
 		userPayload.setUsername(faker.name().username());
 		userPayload.setFirstName(faker.name().firstName());
@@ -28,19 +31,24 @@ public class UserTests {
 		userPayload.setEmail(faker.internet().safeEmailAddress());
 		userPayload.setPassword(faker.internet().password(5, 10));
 		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		
+		logger = LogManager.getLogger(this.getClass());
 	}
 	
 	
 	@Test(priority=1)
 	public void testPostUser() {
+		logger.info("******creating user*********");
 		Response response= UserEndpoints.createUser(userPayload);
 		response.then().log().all();
 		
 		Assert.assertEquals(response.getStatusCode(),200);
+		logger.info("******user created*********");
 	}
 	
 	@Test(priority=2)
 	public void testGetUserByName() {
+		logger.info("******getting user*********");
 		Response response= UserEndpoints.readUser(userPayload.getUsername());
 		response.then().log().all();
 		System.out.println("Email id is -> "+ response.jsonPath().get("email"));
@@ -66,14 +74,17 @@ public class UserTests {
 		responseAfterUpdate.then().log().all();
 					
 		Assert.assertEquals(response.getStatusCode(),200);
+		logger.info("******user updated*********");
 	}
 
 	@Test(priority=4)
 	public void testDeleteUserByName() {
+		logger.info("******deleting user*********");
 		Response response= UserEndpoints.deleteUser(userPayload.getUsername());
 		response.then().log().all();
 		
 		Assert.assertEquals(response.getStatusCode(),200);
+		logger.info("******user deleted*********");
 	}
 	
 		
